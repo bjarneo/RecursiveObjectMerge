4129801fa1c295c34d96691a107ec42d65600cb4
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-11-24 20:41:51 +0100

    test and package update

--- test.js
+++ test.js
@@ -11,13 +11,11 @@ describe('#RecursiveObjectMerge', function() {
             { b: { a: { a: 'a', e: 'e', c: ['c', 'a', 'b'] } } }
         );
 
-        merged.should.deep.equal(
-            {
+        merged.should.deep.equal({
                 a: { a: 'a', b: 'b', c: [ 'a', 'b', 'c' ] },
                 b: { a: { a: 'a', b: 'b', c: ['c', 'a', 'b'], e: 'e' } },
                 c: [ 'a', 'b', 'c' ],
                 d: 'd'
-            }
-        );
+        });
     });
 });
